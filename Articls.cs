a05057949f0ba559401febd449a6adf6eec181f0
Ahmed757448 <68530667+Ahmed757448@users.noreply.github.com>
2020-07-20 08:24:11 +0200

    Articls
    Art

---
+++ Articls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jornal.Models
+{
+    public class Articls
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Describtion { get; set; }
+        public string AuthorName { get; set; }
+
+    }
+}
